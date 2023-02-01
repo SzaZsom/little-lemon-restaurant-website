ff01e39343889e2696c6f25bcf5c4be7c113ab24
Zsombor Szabados <98755213+SzaZsom@users.noreply.github.com>
2023-02-01 12:38:29 +0100

    Implemented the basic structure of the booking form

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage"
 import BookingPage from "./pages/BookingPage"
 import Nav from './Nav'
 import Footer from './Footer'
+import AboutPage from './pages/AboutPage'
 
 function Routing() {
   return (
@@ -12,6 +13,7 @@ function Routing() {
         <Routes>
             <Route path="/" element={<HomePage />}></Route>
             <Route path="/booking" element={<BookingPage />}></Route>
+            <Route path="/about" element={<AboutPage />}></Route>
         </Routes>
         <Footer />
     </Router>
