a249adc8dd419f40da819c08b478edb41d6b8b63
Zsombor Szabados <98755213+SzaZsom@users.noreply.github.com>
2023-02-08 12:46:51 +0100

    Styled the Confirmation Page

--- src/components/pages/ConfirmationPage.js
+++ src/components/pages/ConfirmationPage.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import "./ConfirmationPage.css"
 
 function ConfirmationPage() {
   return (
-    <h1 className='heading_title'>Your Reservation is confirmed!</h1>
+    <header className="container confirmation-header">
+        <img className="confirmation-image" src={require('../../assets/food.jpg')} alt="Little Lemon Ingredients"></img>
+        <div class="confirmation--text">
+            <section className="reserve-header-text">
+                <h1 className='heading_title'>Your Reservation is Confirmed!</h1>
+                <h4 className='heading_subTitle'>A confirmation message has been sent to your email.</h4>
+                <h4 className='heading_subTitle heading_subTitle--thanks'>Thanks for dining with us!</h4>
+            </section>
+            <section className="redirect-buttons">
+                <Link className="btn btn--noDecortion" to="/">Home Page</Link>
+            </section>
+        </div>
+    </header>
   )
 }
 
